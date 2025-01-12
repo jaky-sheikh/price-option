95eee6d8c038d3916dba1f0579fc64120a0e383f
jaky-sheikh <jaky@eu4m.eu>
2025-01-12 07:43:18 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import NavBar from './components/NavBar/NavBar'
+import DaisyNav from './components/DaisyNav/DaisyNav'
+
+function App() {
+
+
+  return (
+    <>
+      <NavBar></NavBar>
+      <DaisyNav></DaisyNav>
+      <h1 className='text-3xl font-bold bg-rose-400'>Price option</h1>
+
+
+    </>
+  )
+}
+
+export default App
